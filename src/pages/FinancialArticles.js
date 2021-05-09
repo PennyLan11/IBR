98049b7ff5984c5b3413f7d35da71fd6693c2b84
PennyLan11 <78224955+PennyLan11@users.noreply.github.com>
2021-05-09 11:22:22 -0700

    Create FinancialArticles.js

---
+++ src/pages/FinancialArticles.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const FinancialArticles = () => {
+    return (
+        <div
+            style= {{
+                display: 'flex',
+                justifyContent: 'Right',
+                alignItems: 'Right',
+                height: '100vh'
+            }}
+        >
+            <h1> Financial Articles. </h1>
+        </div>
+    );
+};
+
+export default FinancialArticles;
